d9826062bd9f802e5b9bbce7276ebb682532feb4
soul4rent <kyle.a.wadsworth@gmail.com>
2018-01-22 20:51:26 -0800

    First Commit

---
+++ dealbot.py
@@ -0,0 +1,52 @@
+import praw
+import time
+from datetime import datetime
+
+import os, sys #import from parent directory
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import secure
+#Name misleading.
+#Literally just a python file in the parent directory
+#named "secure.py" with four plaintext variables.
+#On the local machine only, referenced by this script.
+#It's not secure, so make a throwaway account to use.
+
+
+freegames = [] #make a string list so not spammed with free games
+
+
+while True: #run forever until forced stop. Doesn't matter if user friendly. Only for my personal use.   
+
+    #relogin every 60 seconds to prevent timeouts
+    bot = praw.Reddit(user_agent='GameDealBot v0.1',
+                  client_id=secure.client_id,
+                  client_secret=secure.client_secret,
+                  username=secure.user,
+                  password=secure.password)
+
+    #multireddit with deals from various subreddits
+    subreddit = bot.subreddit("AndroidGameDeals+GameDeals+ebookdeals+FreeGamesOnAndroid")
+
+
+    for submission in subreddit.hot(limit=50):
+
+        title = submission.title.lower() #sets the title to lower case to make string manipulation more friendly
+
+        #help make code look cleaner
+        subreddit = str(submission.subreddit).lower()
+        score = submission.score
+        
+        #if the item isn't terrible
+        if (score > 20 and subreddit == "gamedeals") or (score > 3 and subreddit == "androidgamedeals") or (score > 3 and subreddit == "freegamesonandroid") or (subreddit == "ebookdeals"): 
+            #trying to eliminate false positives (todo: use regex if list of keywords gets too long)
+            if (title.find('free') != -1 or title.find('100%') != -1) and (title.find('free shipping') == -1) and (title.find('free weekend') == -1) and (title.find('free to play') == -1):
+                if freegames.count(title) == 0: #game not already in the list of free games
+                    freegames.append(title)
+                    print("=========================")
+                    print("Title: ", submission.title)
+                    print("Score: ", submission.score)
+                    print(submission.subreddit)
+                    print("Post created ", datetime.fromtimestamp(submission.created)) #convert UTC time when post created to something readable
+                    print("=========================\n")
+
+    time.sleep(60) #sleep for 60 seconds
---
+++ secure_example.py
@@ -0,0 +1,5 @@
+#Brought to you by plaintext security
+client_id='whatever reddit gives you when you register"
+client_secret='whatever reddit gives you when you register'
+user = 'username_here'
+password = 'password_here'
